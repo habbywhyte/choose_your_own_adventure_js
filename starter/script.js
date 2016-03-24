2e52c10a6016cd10bab5bb65c734a9e459324404
Habby Whyte <bahybaj@hotmail.com>
2016-03-24 03:46:37 -0400

    edited wakeUpInput options

--- starter/script.js
+++ starter/script.js
@@ -1,33 +1,46 @@
-// put code here!
-var tantrumFree = function() {
-  console.log("**************************")
-}
+// // put code here!
+// var tantrumFree = function() {
+//   console.log("**************************")
+// }
 console.log ("You've overslept one morning and realized you have 25 minutes to get yourself and your kindergartener out the door")
-var wakeUpInput = prompt("You can either jolt out of bed or push the snooze button") (Please enter 'j' or 's')
-
-if (wakeUpInput) == "j"{
-  console.log("You jolt out of bed and begin dressing your kindergartener, when she decides she doesn't want to wear your option but rather her polka-dot pink Doc McStuffin shirt with her green striped duck pants."
+var wakeUpInput = prompt("You can either wake up or push the snooze button.  (Please enter 'w' or 's')")
+//
+if (wakeUpInput == "w"){
+  console.log("You jolt out of bed and begin dressing your kindergartener, when all of the sudden, she decides she doesn't want to wear your option but rather her polka-dot pink Doc McStuffin shirt with her green striped duck pants.")
+//
+  var reponseToToddler = prompt("You realize that this very moment could make or break her blossoming kindergarten reputation and decide to\n('a') - argue with her and stand your ground. We're talking reputation here people!...hers and yours!\n('g') - give in, toddlers have no reputation.\n('n') - negotiate with her.  She may know the latest trends that you may not be hip to")
 }
-var (reponseToToddler) = prompt("You realize that this very moment could make or break her blossoming kindergarten reputation" and decide to \n('a') - argue with her. We're talking reputation here people!...hers and yours!\n('g') - give in, toddlers have no reputation.  This may actually increase her self-confidence, while the other parents question your parenting skills\n('n')- negotiate with her.  She may know the latest trends that you may not be hip to and needs just a little guidance.")
-
-if(responseToToddler == 'a'){
-  console.log("You argue tooth and nail with her, when you realize you've wasted time, increased your blood pressure and will inevitably be late")
+  if(responseToToddler == "a") {
+    console.log("You argue tooth and nail with her, when you realize you've wasted too much time, increased your blood pressure and will inevitably be late")
+}
+  else if (responseToToddler == 'g'){
+  console.log("With extra vigor, she happily puts on her clothes and brushes her teeth, saving you the extra minutes to brush your own teeth.  Who cares what she wears anyways, you say to yourself")
   buffer()
 }
-
-else if (responseToToddler == 'g'){
-  console.log("With extra vigor, she happily puts on her clothes, saving you the extra minutes to brush your teeth.  Who cares what she wears anyways, you say to yourself")
-  buffer()
-  var toddlerReaction == prompt ("She asks you what's for breakfast?"\n('p') - Pancakes\n('o')'Not Pancakes');
-  if (responseToToddler =='p') {
-    console.log("She gives you her toothiest grin and runs down the stairs.  You follow her down the stairs, to pop in her pancakes in the oven, saving you a minute to finish getting dressed.");
-    tantrumFree();
-  }
-else if (responseToToddler =='o'){
-  console.log("Your toddler lets out a loud shriek and begins to whine.  She then refuses to take a bite, making you late.");
-  bumper();
+  else if (responseToToddler == 'n'){
+    console.log("Lately, your latest fashion sense hasn't lent you any favors which mainly consisted of your neon green yoga pants and your gray hoodie.  No wonder your toddler may have her doubts, you think to yourself.  You try anyway and suggest she wears her hot purple pants with her pink polka dot Doc McStuffin shirt, hoping that your inner fashionista hasn't completely dissipated and might prevail");
 }
+var toddlerReaction == prompt ("She asks you what's for breakfast?\n('p') - pancakes\n('n')- not pancakes")
+if (toddlerReaction ==)
 
-else if (wakeUpInput) == "s"{
-  console.log("You groggily push the snooze button, surrending to the fact, that you'll be late regardless. Only to have your toddler jump onto your bed 3.5 seconds later, shaking all hope of gaining that extra 10 minutes of sleep")
+
+else if (wakeUpInput == "s"){
+  console.log("You groggily push the snooze button, surrending to the fact that you'll be late regardless. Only to have your toddler jump onto your bed 3.5 seconds later, shaking all hope of gaining that extra 10 minutes of sleep")
 }
+//
+//
+// }
+//
+//
+//
+//   if (responseToToddler == 'p'){
+//     console.log("She gives you her toothiest grin and runs down the stairs.  You follow her down the stairs, to pop in her pancakes in the oven, saving you a minute to finish getting dressed.");
+//     tantrumFree();
+//   }
+// else if (responseToToddler =='o'){
+//   console.log("Your toddler lets out a loud shriek and begins to stomp her foot and whine about how she hates [any food]. She then refuses to take a bite, making you late.");
+//   bumper();
+// }
+//
+// var toddlerReaction == prompt ("After gobbling up her pancakes, you proceed to give her some orange juice.  Do you pour it in a green cup (g) or yellow cup (y)?")\n('g') -
+//
